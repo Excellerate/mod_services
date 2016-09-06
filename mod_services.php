a3c9fc61abdf119255dec04caefc17bfe38b3edb
Derrick Egersdorfer <derrick@egersdorfer.co.za>
2016-09-06 09:39:51 +0200

    Init commit

---
+++ mod_services.php
@@ -0,0 +1,88 @@
+<div id="services" class="ui compact center aligned equal height grid">
+	<div class="ui twelve wide column">
+		<div class="ui compact center aligned <?= IS_MOBILE ? 'one' : 'three' ?> column grid">
+			<div class="ui column">
+				<div class="ui service segment <?= ! IS_MOBILE ? 'a' : ''; ?>">
+					<div>
+						<img class="ui centered image" <?= IS_MOBILE ? '' : 'height="60px"' ?> src="<?= JUri::base(); ?>templates/cwe/assets/img/logo.png">
+					</div>
+					<div class="info">
+						CRES, Advisory, Valuations Property management, Facilities management
+					</div>
+				</div>
+			</div>
+			<div class="ui column">
+				<div class="ui service segment <?= ! IS_MOBILE ? 'b' : ''; ?>">
+					<div>
+						<img class="ui centered image" <?= IS_MOBILE ? '' : 'height="40px"' ?> src="<?= JUri::base(); ?>templates/cwe/assets/img/groupLogos/enforce-dark.png">
+					</div>
+					<div class="info">
+						Security Services
+					</div>
+				</div>
+			</div>
+			<div class="ui column">
+				<div class="ui service segment <?= ! IS_MOBILE ? 'c' : ''; ?>">
+					<div>
+						<img class="ui centered image" <?= IS_MOBILE ? '' : 'height="40px"' ?> src="<?= JUri::base(); ?>templates/cwe/assets/img/groupLogos/interpark.png">
+					</div>
+					<div class="info">
+						Access Control &amp; Parking Management
+					</div>
+				</div>
+			</div>
+			<div class="ui column">
+				<div class="ui service segment <?= ! IS_MOBILE ? 'd' : ''; ?>">
+					<div>
+						<img class="ui centered image" <?= IS_MOBILE ? '' : 'height="40px"' ?> src="<?= JUri::base(); ?>templates/cwe/assets/img/groupLogos/profica.png">
+					</div>
+					<div class="info">
+						Project Management
+					</div>
+				</div>
+			</div>
+			<div class="ui column">
+				<div class="ui service segment <?= ! IS_MOBILE ? 'e' : ''; ?>">
+					<div>
+						<img class="ui centered image" <?= IS_MOBILE ? '' : 'height="60px"' ?> src="<?= JUri::base(); ?>templates/cwe/assets/img/groupLogos/spark.png">
+					</div>
+					<div class="info">
+						Parking Management
+					</div>
+				</div>
+			</div>
+			<div class="ui column">
+				<div class="ui service segment <?= ! IS_MOBILE ? 'f' : ''; ?>">
+					<div>
+						<img class="ui centered image" <?= IS_MOBILE ? '' : 'width="100px"' ?> src="<?= JUri::base(); ?>templates/cwe/assets/img/groupLogos/sterikleen.png"><br>
+						<img class="ui centered image" <?= IS_MOBILE ? '' : 'width="100px"' ?> src="<?= JUri::base(); ?>templates/cwe/assets/img/groupLogos/eradico.png"><br>
+						<img class="ui centered image" <?= IS_MOBILE ? '' : 'width="100px"' ?> src="<?= JUri::base(); ?>templates/cwe/assets/img/groupLogos/katanga.png">
+					</div>
+					<div class="info">
+						Cleaning &amp; Pest Control
+					</div>
+				</div>
+			</div>
+			<div class="ui column">
+				<div class="ui service segment <?= ! IS_MOBILE ? 'g' : ''; ?>">
+					<div>
+						<img class="ui centered image" <?= IS_MOBILE ? '' : 'height="60px"' ?> src="<?= JUri::base(); ?>templates/cwe/assets/img/groupLogos/fresh.png">
+					</div>
+					<div class="info">
+						Food stations
+					</div>
+				</div>
+			</div>
+			<div class="ui column">
+				<div class="ui service segment <?= ! IS_MOBILE ? 'h' : ''; ?>">
+					<div>
+						<img class="ui centered image" <?= IS_MOBILE ? '' : 'height="60px"' ?> src="<?= JUri::base(); ?>templates/cwe/assets/img/groupLogos/first.png">
+					</div>
+					<div class="info">
+						Integrated &amp; Multi-disiplinary Management
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
